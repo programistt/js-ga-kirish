d2be81b72a548749acee887072ca9f891188677e
Islom Toirjonov <toirjonovislomjon3@gmail.com>
2024-04-23 23:48:00 +0500

    done

---
+++ index.js
@@ -0,0 +1,5 @@
+let ism = prompt("ismingiz nima ");
+let yosh = prompt("yoshingiz ");
+let yonalish = prompt("yonalishingiz ");
+let address = prompt("addresingiz ");
+alert( ism + " " + yosh + "yosh - " + yonalish + " " + address + "!");
